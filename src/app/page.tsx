43223bc6eead3eeb5d83e017d5958cd972be6682
genesis331 <cheah3838@gmail.com>
2024-11-27 01:16:53 +0800

    complete home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,37 +2,37 @@ import {Card, CardContent} from "@/components/ui/card";
 
 export default function Index() {
   return (
-      <div className="min-h-screen flex flex-col">
-          <div>Welcome, Ahmad Iskandar</div>
-          <div>
-              <Card>
-                  <CardContent className="p-0">
+      <div className="min-h-screen flex flex-col px-6 py-10 gap-6">
+          <div className="text-3xl font-bold">Welcome, <span className="text-blue-700">Ahmad Iskandar</span></div>
+          <div className="flex flex-col gap-5">
+              <Card className="shadow-none">
+                  <CardContent className="px-5 py-4">
                       <div>
-                          <div>Total Asset</div>
+                          <div className="font-semibold">Total Asset</div>
                           <div></div>
                       </div>
                   </CardContent>
               </Card>
-              <Card>
-                  <CardContent className="p-0">
+              <Card className="shadow-none">
+                  <CardContent className="px-5 py-4">
                       <div>
-                          <div>Debt Repayment Progress</div>
+                          <div className="font-semibold">Debt Repayment Progress</div>
                           <div></div>
                       </div>
                   </CardContent>
               </Card>
-              <Card>
-                  <CardContent className="p-0">
+              <Card className="shadow-none">
+                  <CardContent className="px-5 py-4">
                       <div>
-                          <div>Debt vs Asset by Month</div>
+                          <div className="font-semibold">Debt vs Asset by Month</div>
                           <div></div>
                       </div>
                   </CardContent>
               </Card>
-              <Card>
-                  <CardContent className="p-0">
+              <Card className="shadow-none">
+                  <CardContent className="px-5 py-4">
                       <div>
-                          <div>Debt Ratios</div>
+                          <div className="font-semibold">Debt Ratios</div>
                           <div></div>
                       </div>
                   </CardContent>
