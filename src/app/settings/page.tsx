a513ed7c8e21295ce45a08a1f9821c865aea6a5f
genesis331 <cheah3838@gmail.com>
2024-11-26 23:34:29 +0800

    add modules, fonts & complete settings page

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,130 +1,132 @@
 import {Card, CardContent} from "@/components/ui/card";
 import {Button} from "@/components/ui/button";
-import {PencilIcon, Trash2Icon} from "lucide-react";
+import {PencilIcon} from "lucide-react";
 import {Switch} from "@/components/ui/switch";
 
 export default function Index() {
   return (
-      <div className="min-h-screen flex flex-col">
-          <div>Settings</div>
-          <div>
-          <div>Edit Your Info</div>
-              <div className="flex flex-col">
-                  <Card>
-                      <CardContent className="p-0">
+      <div className="min-h-screen flex flex-col px-6 py-10 gap-6">
+          <div className="text-4xl font-black">Settings</div>
+          <div className="flex flex-col gap-3.5">
+              <div className="font-semibold">Edit Your Info</div>
+              <div className="flex flex-col gap-4">
+                  <Card className="shadow-none">
+                      <CardContent className="px-5 py-4">
                           <div className="flex items-center">
                               <div className="flex-1">
                                   <div>Ahmad Iskandar</div>
-                                  <div>Full Name</div>
+                                  <div className="text-xs text-gray-500">Full Name</div>
                               </div>
                               <div>
-                                  <Button variant="ghost"><PencilIcon /></Button>
+                                  <Button className="[&_svg]:size-6 px-2 text-gray-500" variant="ghost"><PencilIcon /></Button>
                               </div>
                           </div>
                       </CardContent>
                   </Card>
-                  <Card>
-                      <CardContent className="p-0">
+                  <Card className="shadow-none">
+                      <CardContent className="px-5 py-4">
                           <div className="flex items-center">
                               <div className="flex-1">
                                   <div>ahmad@example.com</div>
-                                  <div>Email</div>
+                                  <div className="text-xs text-gray-500">Email</div>
                               </div>
                               <div>
-                                  <Button variant="ghost"><PencilIcon /></Button>
+                                  <Button className="[&_svg]:size-6 px-2 text-gray-500" variant="ghost"><PencilIcon /></Button>
                               </div>
                           </div>
                       </CardContent>
                   </Card>
               </div>
               <div>
-                  <Button>Change Password?</Button>
+                  <Button className="w-full font-semibold bg-blue-700 hover:bg-blue-700/80" size="lg">Change Password</Button>
               </div>
           </div>
-          <div>
-              <div>Manage Linked Accounts</div>
-              <div className="flex flex-col">
-                  <Card>
-                      <CardContent className="p-0">
+          <div className="flex flex-col gap-3.5">
+              <div className="font-semibold">Manage Linked Accounts</div>
+              <div className="flex flex-col gap-4">
+                  <Card className="shadow-none">
+                      <CardContent className="px-5 py-4">
                           <div className="flex items-center">
                               <div className="flex-1">
                                   <div>Maybank</div>
-                                  <div>xxxxxxxx13</div>
+                                  <div className="text-xs text-gray-500">xxxxxxxx13</div>
                               </div>
                               <div>
-                                  <Button variant="ghost"><Trash2Icon /></Button>
+                                  <Button className="[&_svg]:size-6 px-2 text-gray-500" variant="ghost"><PencilIcon /></Button>
                               </div>
                           </div>
                       </CardContent>
                   </Card>
-                  <Card>
-                      <CardContent className="p-0">
+                  <Card className="shadow-none">
+                      <CardContent className="px-5 py-4">
                           <div className="flex items-center">
                               <div className="flex-1">
                                   <div>Touch n’ Go E-Wallet</div>
-                                  <div>601xxxxxxxx27</div>
+                                  <div className="text-xs text-gray-500">601xxxxxxxx27</div>
                               </div>
                               <div>
-                                  <Button variant="ghost"><Trash2Icon /></Button>
+                                  <Button className="[&_svg]:size-6 px-2 text-gray-500" variant="ghost"><PencilIcon /></Button>
                               </div>
                           </div>
                       </CardContent>
                   </Card>
               </div>
               <div>
-                  <Button>Add Accounts</Button>
+                  <Button className="w-full font-semibold bg-blue-700 hover:bg-blue-700/80" size="lg">Add Accounts</Button>
               </div>
           </div>
-          <div>
-              <div>Alerts & Notification</div>
-              <div>
+          <div className="flex flex-col gap-3.5">
+              <div className="font-semibold">Alerts & Notification</div>
+              <div className="flex flex-col gap-4">
                   <div className="flex">
-                      <div className="flex-1">
-                          <div>Alert</div>
-                          <div>Notifications for prepayment penalties or approaching grace period expirations.</div>
+                      <div className="flex-1 flex flex-col gap-1.5">
+                          <div className="text-sm">Alert</div>
+                          <div className="text-xs text-gray-500">Notifications for prepayment penalties or approaching grace period
+                              expirations.
+                          </div>
                       </div>
                       <div>
-                          <Switch />
+                          <Switch/>
+                      </div>
+                  </div>
+                  <div className="flex">
+                      <div className="flex-1 flex flex-col gap-1.5">
+                          <div className="text-sm">Offers & Discount Notification</div>
+                          <div className="text-xs text-gray-500">Notifications for prepayment penalties or approaching grace period
+                              expirations.
+                          </div>
+                      </div>
+                      <div>
+                          <Switch/>
                       </div>
                   </div>
               </div>
-              <div>
-                  <div className="flex">
-                      <div className="flex-1">
-                          <div>Offers & Discount Notification</div>
-                          <div>Notifications for prepayment penalties or approaching grace period expirations.</div>
-                      </div>
-                      <div>
-                          <Switch />
-                      </div>
-                  </div>
-              </div>
           </div>
-          <div>
-              <div>Thresholds</div>
-              <div className="flex flex-col">
-                  <Card>
-                      <CardContent className="p-0">
+          <div className="flex flex-col gap-3.5">
+              <div className="font-semibold">Thresholds</div>
+              <div className="flex flex-col gap-4">
+                  <Card className="shadow-none">
+                      <CardContent className="px-5 py-4">
                           <div className="flex items-center">
                               <div className="flex-1">
                                   <div>40% of Current Assets</div>
-                                  <div>Debt-to-Asset Ratio Alert</div>
+                                  <div className="text-xs text-gray-500">Debt-to-Asset Ratio Alert</div>
                               </div>
                               <div>
-                                  <Button variant="ghost"><PencilIcon /></Button>
+                                  <Button className="[&_svg]:size-6 px-2 text-gray-500" variant="ghost"><PencilIcon/></Button>
                               </div>
                           </div>
                       </CardContent>
                   </Card>
-                  <Card>
-                      <CardContent className="p-0">
+                  <Card className="shadow-none">
+                      <CardContent className="px-5 py-4">
                           <div className="flex items-center">
                               <div className="flex-1">
                                   <div>RM800</div>
-                                  <div>Amount Kept from Debt Repayment</div>
+                                  <div className="text-xs text-gray-500">Amount Kept from Debt Repayment</div>
                               </div>
                               <div>
-                                  <Button variant="ghost"><PencilIcon /></Button>
+                                  <Button className="[&_svg]:size-6 px-2 text-gray-500" variant="ghost"><PencilIcon /></Button>
                               </div>
                           </div>
                       </CardContent>
@@ -132,7 +134,7 @@ export default function Index() {
               </div>
           </div>
           <div>
-              <Button>Delete Account</Button>
+              <Button className="w-full font-semibold border-destructive text-destructive hover:text-destructive bg-destructive/20 hover:bg-destructive/10" size="lg" variant="outline">Delete Account</Button>
           </div>
       </div>
   );
