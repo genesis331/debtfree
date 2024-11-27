504aaa31a4a2bab22650a817b392d3c7401b53fd
Jun Yi <limjunyi03@gmail.com>
2024-11-27 15:56:14 +0800

    Added links to external URLs

--- src/app/investment/page.tsx
+++ src/app/investment/page.tsx
@@ -1,55 +1,85 @@
-import {Card, CardContent} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function Index() {
-  return (
-      <div className="min-h-screen flex flex-col px-6 pb-10 gap-6">
-          <div className="text-3xl font-bold">Grow Your Money</div>
-          <div className="flex flex-col gap-3.5">
-              <div className="font-semibold">Low-Risk Investments</div>
-              <div className="flex flex-col gap-4">
-                  <Card className="shadow-none">
-                      <CardContent className="px-5 py-4">
-                          <div>Maybank Fixed Deposit</div>
-                          <div className="text-xs text-zinc-500">up to 3.80% p.a.</div>
-                      </CardContent>
-                  </Card>
-                  <Card className="shadow-none">
-                      <CardContent className="px-5 py-4">
-                          <div>Malaysian Government Securities</div>
-                          <div className="text-xs text-zinc-500">up to 3.81% p.a.</div>
-                      </CardContent>
-                  </Card>
-              </div>
-          </div>
-          <div className="flex flex-col gap-3.5">
-              <div className="font-semibold">Learn Investment</div>
-              <div className="flex flex-col gap-4">
-                  <Card className="shadow-none">
-                      <CardContent className="px-5 py-4">
-                          <div>Decoding Financial Earning Reports</div>
-                          <div className="text-xs text-zinc-500">Financial Reports</div>
-                      </CardContent>
-                  </Card>
-                  <Card className="shadow-none">
-                      <CardContent className="px-5 py-4">
-                          <div>Investing in Dividend Stocks</div>
-                          <div className="text-xs text-zinc-500">Diversify Your Portfolio</div>
-                      </CardContent>
-                  </Card>
-                  <Card className="shadow-none">
-                      <CardContent className="px-5 py-4">
-                          <div>Beginner’s Guide to ETFs</div>
-                          <div className="text-xs text-zinc-500">ETF Investing</div>
-                      </CardContent>
-                  </Card>
-                  <Card className="shadow-none">
-                      <CardContent className="px-5 py-4">
-                          <div>How to Reduce Investment Risk</div>
-                          <div className="text-xs text-zinc-500">Risk Management</div>
-                      </CardContent>
-                  </Card>
-              </div>
-          </div>
-      </div>
-  );
+    return (
+        <div className="min-h-screen flex flex-col px-6 pb-10 gap-6">
+            <div className="text-3xl font-bold">Grow Your Money</div>
+            <div className="flex flex-col gap-3.5">
+                <div className="font-semibold">Low-Risk Investments</div>
+                <div className="flex flex-col gap-4">
+                    <a 
+                        href="https://www.maybank2u.com.my/maybank2u/malaysia/en/personal/accounts/fixed_deposits/efixed_deposit.page"
+                        target="_blank"
+                    >
+                        <Card className="shadow-none">
+                            <CardContent className="px-5 py-4">
+                                <div>Maybank Fixed Deposit</div>
+                                <div className="text-xs text-zinc-500">up to 3.80% p.a.</div>
+                            </CardContent>
+                        </Card>
+                    </a>
+                    <a 
+                        href="https://financialmarkets.bnm.gov.my/types-of-securities"
+                        target="_blank"
+                    >
+                        <Card className="shadow-none">
+                            <CardContent className="px-5 py-4">
+                                <div>Malaysian Government Securities</div>
+                                <div className="text-xs text-zinc-500">up to 3.81% p.a.</div>
+                            </CardContent>
+                        </Card>
+                    </a>
+                </div>
+            </div>
+            <div className="flex flex-col gap-3.5">
+                <div className="font-semibold">Learn Investment</div>
+                <div className="flex flex-col gap-4">
+                    <a
+                        href="https://www.moomoo.com/my/learn/detail-introduction-to-earnings-report-58661-220562077"
+                        target="_blank"
+                    >
+                        <Card className="shadow-none">
+                            <CardContent className="px-5 py-4">
+                                <div>Decoding Financial Earning Reports</div>
+                                <div className="text-xs text-zinc-500">Financial Reports</div>
+                            </CardContent>
+                        </Card>
+                    </a>
+                    <a
+                        href="https://www.moomoo.com/my/learn/detail-dividend-stocks-a-haven-amid-market-volatility-55842-220412022"
+                        target="_blank"
+                    >
+                        <Card className="shadow-none">
+                            <CardContent className="px-5 py-4">
+                                <div>Investing in Dividend Stocks</div>
+                                <div className="text-xs text-zinc-500">Diversify Your Portfolio</div>
+                            </CardContent>
+                        </Card>
+                    </a>
+                    <a
+                        href="https://www.moomoo.com/my/learn/detail-what-is-an-etf-117240-240629222"
+                        target="_blank"
+                    >
+                        <Card className="shadow-none">
+                            <CardContent className="px-5 py-4">
+                                <div>Beginner’s Guide to ETFs</div>
+                                <div className="text-xs text-zinc-500">ETF Investing</div>
+                            </CardContent>
+                        </Card>
+                    </a>
+                    <a
+                        href="https://www.moomoo.com/my/learn/detail-01-what-is-investment-risk-116333-230961027"
+                        target="_blank"
+                    >
+                        <Card className="shadow-none">
+                            <CardContent className="px-5 py-4">
+                                <div>How to Reduce Investment Risk</div>
+                                <div className="text-xs text-zinc-500">Risk Management</div>
+                            </CardContent>
+                        </Card>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/app/refinancing/page.tsx
+++ src/app/refinancing/page.tsx
@@ -1,72 +1,87 @@
-import {Card, CardContent} from "@/components/ui/card";
-import {FilterIcon} from "lucide-react";
-import {Button} from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { FilterIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default function Index() {
-  return (
-      <div className="min-h-screen flex flex-col px-6 pb-10 gap-6">
-          <div className="text-3xl font-bold">Refinancing Options</div>
-          <div className="flex flex-col gap-3.5">
-              <div>Recommended for you</div>
-              <div className="flex flex-col gap-4">
-                  <Card className="shadow-none">
-                      <CardContent className="px-5 py-4">
-                          <div className="flex flex-col gap-4">
-                              <div className="flex flex-col gap-1">
-                                  <div className="text-xs text-zinc-500">Housing Loan</div>
-                                  <div>CIMB Malaysia My Second Home</div>
-                              </div>
-                              <div className="text-3xl font-medium pb-1">RM1,700/mo</div>
-                              <div className="flex justify-between text-xs text-zinc-500">
-                                  <div>Max 35 years</div>
-                                  <div>from 2.15% p.a.</div>
-                              </div>
-                          </div>
-                      </CardContent>
-                  </Card>
-              </div>
-          </div>
-          <div className="flex flex-col gap-3.5">
-              <div className="flex justify-between">
-                  <div>Popular offers</div>
-                  <Button variant="ghost" className="px-0 py-0 h-min text-zinc-500">
-                      <FilterIcon/> All Loans
-                  </Button>
-              </div>
-              <div className="flex flex-col gap-4">
-                  <Card className="shadow-none">
-                      <CardContent className="px-5 py-4">
-                          <div className="flex flex-col gap-4">
-                              <div className="flex flex-col gap-1">
-                                  <div className="text-xs text-zinc-500">Housing Loan</div>
-                                  <div>Alliance Bank Personal Loan</div>
-                              </div>
-                              <div className="text-3xl font-medium pb-1">RM466.58/mo</div>
-                              <div className="flex justify-between text-xs text-zinc-500">
-                                  <div>Max 7 years</div>
-                                  <div>from 4.99% p.a.</div>
-                              </div>
-                          </div>
-                      </CardContent>
-                  </Card>
-                  <Card className="shadow-none">
-                      <CardContent className="px-5 py-4">
-                          <div className="flex flex-col gap-4">
-                              <div className="flex flex-col gap-1">
-                                  <div className="text-xs text-zinc-500">Vehicle Loan</div>
-                                  <div>HLB Auto Loan Fixed Rate</div>
-                              </div>
-                              <div className="text-3xl font-medium pb-1">RM512.57/mo</div>
-                              <div className="flex justify-between text-xs text-zinc-500">
-                                  <div>Max 9 years</div>
-                                  <div>2.80% p.a.</div>
-                              </div>
-                          </div>
-                      </CardContent>
-                  </Card>
-              </div>
-              <div className="text-center font-medium text-zinc-500 text-sm">View more offers</div>
-          </div>
-      </div>
-  );
+    return (
+        <div className="min-h-screen flex flex-col px-6 pb-10 gap-6">
+            <div className="text-3xl font-bold">Refinancing Options</div>
+            <div className="flex flex-col gap-3.5">
+                <div>Recommended for you</div>
+                <div className="flex flex-col gap-4">
+                    <a
+                        href="https://www.cimb.com.my/en/personal/day-to-day-banking/financing/property-financing/malaysia-my-second-home.html"
+                        target="_blank"
+                    >
+                        <Card className="shadow-none">
+                            <CardContent className="px-5 py-4">
+                                <div className="flex flex-col gap-4">
+                                    <div className="flex flex-col gap-1">
+                                        <div className="text-xs text-zinc-500">Housing Loan</div>
+                                        <div>CIMB Malaysia My Second Home</div>
+                                    </div>
+                                    <div className="text-3xl font-medium pb-1">RM1,700/mo</div>
+                                    <div className="flex justify-between text-xs text-zinc-500">
+                                        <div>Max 35 years</div>
+                                        <div>from 2.15% p.a.</div>
+                                    </div>
+                                </div>
+                            </CardContent>
+                        </Card>
+                    </a>
+                </div>
+            </div>
+            <div className="flex flex-col gap-3.5">
+                <div className="flex justify-between">
+                    <div>Popular offers</div>
+                    <Button variant="ghost" className="px-0 py-0 h-min text-zinc-500">
+                        <FilterIcon /> All Loans
+                    </Button>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <a
+                        href="https://www.alliancebank.com.my/Personal/Financing/personal-loan-financing"
+                        target="_blank"
+                    >
+                        <Card className="shadow-none">
+                            <CardContent className="px-5 py-4">
+                                <div className="flex flex-col gap-4">
+                                    <div className="flex flex-col gap-1">
+                                        <div className="text-xs text-zinc-500">Housing Loan</div>
+                                        <div>Alliance Bank Personal Loan</div>
+                                    </div>
+                                    <div className="text-3xl font-medium pb-1">RM466.58/mo</div>
+                                    <div className="flex justify-between text-xs text-zinc-500">
+                                        <div>Max 7 years</div>
+                                        <div>from 4.99% p.a.</div>
+                                    </div>
+                                </div>
+                            </CardContent>
+                        </Card>
+                    </a>
+                    <a 
+                        href="https://www.hlb.com.my/en/personal-banking/loans/motor-loan/auto-loans.html/"
+                        target="_blank"
+                    >
+                        <Card className="shadow-none">
+                            <CardContent className="px-5 py-4">
+                                <div className="flex flex-col gap-4">
+                                    <div className="flex flex-col gap-1">
+                                        <div className="text-xs text-zinc-500">Vehicle Loan</div>
+                                        <div>HLB Auto Loan Fixed Rate</div>
+                                    </div>
+                                    <div className="text-3xl font-medium pb-1">RM512.57/mo</div>
+                                    <div className="flex justify-between text-xs text-zinc-500">
+                                        <div>Max 9 years</div>
+                                        <div>2.80% p.a.</div>
+                                    </div>
+                                </div>
+                            </CardContent>
+                        </Card>
+                    </a>
+                </div>
+                <div className="text-center font-medium text-zinc-500 text-sm">View more offers</div>
+            </div>
+        </div>
+    );
 }
