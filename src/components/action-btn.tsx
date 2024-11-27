63993da6d1ad389059a23f85aa0a991ded8eb447
genesis331 <cheah3838@gmail.com>
2024-11-27 13:18:04 +0800

    link to chat page

--- src/components/action-btn.tsx
+++ src/components/action-btn.tsx
@@ -1,10 +1,15 @@
 import {Button} from "@/components/ui/button";
 import {BotMessageSquareIcon} from "lucide-react";
+import Link from "next/link";
 
 export default function ActionButton() {
     return (
-        <div className="fixed bottom-6 right-6">
-            <Button className="[&_svg]:size-7 text-white h-16 w-16 bg-blue-700 hover:bg-blue-700/80 rounded-full"><BotMessageSquareIcon/></Button>
+        <div className="fixed bottom-6 right-6 z-10">
+            <Link href="/chat">
+                <Button className="[&_svg]:size-7 text-white h-16 w-16 bg-blue-700 hover:bg-blue-700/80 rounded-full">
+                    <BotMessageSquareIcon/>
+                </Button>
+            </Link>
         </div>
     )
 }
