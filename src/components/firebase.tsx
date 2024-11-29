b8a64708317c17183dc6fe3c05f0d47ce1549e09
genesis331 <cheah3838@gmail.com>
2024-11-29 16:51:04 +0800

    setup firebase sdk

---
+++ src/components/firebase.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCBvhm91E5wBQawXsg9J7sTyro_djKtzFs",
+    authDomain: "debtfree-5640f.firebaseapp.com",
+    projectId: "debtfree-5640f",
+    storageBucket: "debtfree-5640f.firebasestorage.app",
+    messagingSenderId: "788278871335",
+    appId: "1:788278871335:web:88276917738e5ab03b5502"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
