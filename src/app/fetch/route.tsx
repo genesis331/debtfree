67081f98bd13d248ee74df9f6baea2c2689988d3
genesis331 <cheah3838@gmail.com>
2024-11-30 22:48:16 +0800

    cleanup

--- src/app/fetch/route.tsx
+++ src/app/fetch/route.tsx
@@ -4,7 +4,7 @@ export const runtime = 'edge';
 export async function POST(req: NextRequest) {
     let content = req.body;
 
-    const response = await fetch("https://api.sendgrid.com/v3/mail/send", {
+    await fetch("https://api.sendgrid.com/v3/mail/send", {
         method: "POST",
         headers: {
             "Authorization": `Bearer ${process.env.NEXT_PUBLIC_SENDGRID_API_KEY}`,
