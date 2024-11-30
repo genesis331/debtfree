1ae5dc5807151b2156e7f410deaea7f1393a6c61
genesis331 <cheah3838@gmail.com>
2024-11-30 21:23:08 +0800

    try building prod

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import {NextConfig} from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+    webpack: (config: NextConfig) => {
+        config.resolve.fallback = { fs: false };
+        return config;
+    },
 };
-
-export default nextConfig;
