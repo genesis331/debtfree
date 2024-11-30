cec8bd206b5a44a0201396d3ff752a00673f66b4
genesis331 <cheah3838@gmail.com>
2024-11-30 12:58:26 +0800

    prep form module & complete llm call

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -4,58 +4,169 @@ import {Card, CardContent} from "@/components/ui/card";
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {SendIcon} from "lucide-react";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import {ScrollArea} from "@/components/ui/scroll-area";
+import {collection, getDocs, getFirestore} from "firebase/firestore";
+import app from "@/components/firebase";
 
 type Message = {
     content: string;
-    isUser: boolean;
+    user: number;
+}
+
+interface FinanceDoc {
+    month: { seconds: number };
+    sources: Array<{
+        data: {
+            expense: number;
+            income: number;
+        };
+        source: string;
+    }>;
+}
+
+interface DebtDoc {
+    installment: number;
+    interest_pct: number;
+    name: string;
+    principal: number;
+    remaining: number;
+    term_month: number;
+    type: string;
 }
 
 export default function Index() {
     const apiKey: string = process.env.NEXT_PUBLIC_GEMINI_API_KEY || "";
     const genAI = new GoogleGenerativeAI(apiKey);
-    const model = genAI.getGenerativeModel({
+    const model1 = genAI.getGenerativeModel({
         model: "gemini-1.5-flash",
-        systemInstruction: "Finance data: \n- Date (month)\n- Source (bank / wallet etc)\n- Total income each month\n- Total expense each month\nLoan data: \n- Loan name\n- Loan type\n- Loan term\n- Interest percentage\n- Payment per month\n- Initial amount\n- Remaining amount\n\nYou are a chatbot to answer the question from user. Read the message and determine whether finance or loan or both data are needed. \n\nReturn in this JSON schema:\n{\n\t\"loan\": boolean,\n    \"finance\": boolean\n}",
+        systemInstruction: "Finance data: \n- Date (month)\n- Source (bank / wallet etc)\n- Total income each month\n- Total expense each month\nLoan data: \n- Loan name\n- Loan type\n- Loan term\n- Interest percentage\n- Payment per month\n- Initial amount\n- Remaining amount\n\nYou are a chatbot to answer the question from user. Read the message and determine whether finance or loan or both data are needed or not needed. \n\nReturn in this JSON schema:\n{\n\t\"loan\": boolean,\n    \"finance\": boolean\n}",
     });
-    const generationConfig = {
-        temperature: 1,
-        topP: 0.95,
-        topK: 40,
-        maxOutputTokens: 8192,
-        responseMimeType: "application/json",
-    };
+    const model2 = genAI.getGenerativeModel({
+        model: "gemini-1.5-flash",
+        systemInstruction: "You are a chatbot to answer the question from user. Read the message and the provided user data, then answer the question accordingly. ",
+    });
     const [currentMessage, setCurrentMessage] = useState<string>("");
     const [messages, setMessages] = useState<Message[]>([]);
+    const db = getFirestore(app);
+    const [financeData, setFinanceData] = useState<FinanceDoc[]>([]);
+    const [debtData, setDebtData] = useState<DebtDoc[]>([]);
+    const [financeDataNeeded, setFinanceDataNeeded] = useState<boolean>(false);
+    const [debtDataNeeded, setDebtDataNeeded] = useState<boolean>(false);
 
-    function appendMessage(content: string, isUser: boolean) {
-        setMessages([...messages, {content, isUser}]);
+    const fetchFinanceData = async () => {
+        const financeCollection = collection(db, "finance");
+        const financeSnapshot = await getDocs(financeCollection);
+        const financeData: FinanceDoc[] = financeSnapshot.docs.map((doc) =>
+            doc.data() as FinanceDoc
+        );
+        setFinanceData(financeData);
+    };
+
+    const fetchDebtData = async () => {
+        const debtCollection = collection(db, "debt");
+        const debtSnapshot = await getDocs(debtCollection);
+        const debtData: DebtDoc[] = debtSnapshot.docs.map((doc) =>
+            doc.data() as DebtDoc
+        );
+        setDebtData(debtData);
+    };
+
+    useEffect(() => {
+        fetchFinanceData();
+        fetchDebtData();
+    }, []);
+
+    async function sendToModel(message: string) {
+        const chatSession1 = model1.startChat({generationConfig: {
+                temperature: 1,
+                topP: 0.95,
+                topK: 40,
+                maxOutputTokens: 8192,
+                responseMimeType: "application/json",
+            }});
+        const chatSession2 = model2.startChat({generationConfig: {
+                temperature: 1,
+                topP: 0.95,
+                topK: 40,
+                maxOutputTokens: 8192,
+                responseMimeType: "text/plain",
+            }});
+        let response: {
+            finance: boolean;
+            loan: boolean;
+        } = {
+            finance: false,
+            loan: false,
+        };
+        setMessages([...messages, {content: message, user: 3}]);
+        await chatSession1.sendMessage(message).then(async (res) => {
+            response = JSON.parse(res.response.text());
+            console.log(response);
+            if (response.finance !== financeDataNeeded) {
+                if (!financeDataNeeded && response.finance) {
+                    setMessages([...messages, {content: "Limited finance data will be shared. ", user: 2}]);
+                }
+                setFinanceDataNeeded(response.finance);
+                const financeDataString = financeData.map((data) => {
+                    return `Month: ${new Date(data.month.seconds * 1000).toLocaleString()}\n${data.sources.map((source) => {
+                        return `Source: ${source.source}\nIncome: ${source.data.income}\nExpense: ${source.data.expense}\n`;
+                    }).join("\n")}`;
+                }).join("\n");
+                await chatSession2.sendMessage(financeDataString).then((res) => {
+
+                });
+            }
+            if (response.loan !== debtDataNeeded) {
+                if (!debtDataNeeded && response.loan) {
+                    setMessages([...messages, {content: "Limited debt data will be shared. ", user: 2}]);
+                }
+                setDebtDataNeeded(response.loan);
+                const debtDataString = debtData.map((data) => {
+                    return `Name: ${data.name}\nType: ${data.type}\nTerm: ${data.term_month}\nInterest: ${data.interest_pct}\nPayment: ${data.installment}\nPrincipal: ${data.principal}\nRemaining: ${data.remaining}\n`;
+                }).join("\n");
+                await chatSession2.sendMessage(debtDataString).then((res) => {
+
+                });
+            }
+        });
+        await chatSession2.sendMessage(message).then((res) => {
+            const response = res.response.text();
+            setMessages([...messages, {content: response, user: 1}]);
+        });
     }
 
-    function generateChatBubble(message: string, isUser: boolean, index: number) {
-        return (
-            <div className={`flex ${isUser ? "justify-end" : ""}`} key={index}>
-                <Card className={`shadow-none max-w-[80%] ${isUser ? "bg-zinc-100 border-none" : "bg-transparent"}`}>
-                    <CardContent className="px-5 py-3">
-                        <div className="flex flex-col gap-1">
-                            <div>{message}</div>
-                        </div>
-                    </CardContent>
-                </Card>
-            </div>
-        )
+    function generateChatBubble(message: string, user: number, index: number) {
+        if (user === 1 || user === 3) {
+            return (
+                <div className={`flex ${user === 3 ? "justify-end" : ""}`} key={index}>
+                    <Card className={`shadow-none max-w-[80%] ${user === 3 ? "bg-zinc-100 border-none" : "bg-transparent"}`}>
+                        <CardContent className="px-5 py-3">
+                            <div className="flex flex-col gap-1">
+                                <div>{message}</div>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
+            )
+        } else {
+            return (
+                <div className="flex justify-center text-zinc-500 text-sm py-1.5" key={index}>
+                    {message}
+                </div>
+            )
+        }
     }
 
-  return (
-      <div className="flex-1 flex flex-col px-6 pb-6 gap-6">
+    return (
+        <div className="flex-1 flex flex-col px-6 pb-6 gap-6">
           {
               messages.length ? <div className="flex-1">
                   <ScrollArea className="h-full">
                       <div className="flex flex-col justify-start gap-6">
                           <div className="flex flex-col gap-2">
-                              {messages.map((message, index) => generateChatBubble(message.content, message.isUser, index))}
+                              {messages.map((message, index) => generateChatBubble(message.content, message.user, index))}
                           </div>
                       </div>
                   </ScrollArea>
@@ -107,7 +218,7 @@ export default function Index() {
                              setCurrentMessage(e.target.value)
                          }}/>
                   <Button type="submit" size="lg" className="bg-blue-700 hover:bg-blue-700/80 px-4" onClick={() => {
-                      appendMessage(currentMessage, true);
+                      sendToModel(currentMessage);
                       setCurrentMessage("");
                   }}>
                       <SendIcon/>
