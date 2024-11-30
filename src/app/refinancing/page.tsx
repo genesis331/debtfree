b0df408346ab611044b6ecb44142ce4cd959861f
Jun Yi <limjunyi03@gmail.com>
2024-11-30 11:35:43 +0800

    done show more button, pending view more dialog

--- src/app/refinancing/page.tsx
+++ src/app/refinancing/page.tsx
@@ -1,8 +1,53 @@
+"use client"
+
+import app from "@/components/firebase";
 import { Card, CardContent } from "@/components/ui/card";
 import { FilterIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+interface LoanDoc {
+    maxTenure: string;
+    monthly: string;
+    name: string;
+    rate: string;
+    type: string;
+    url: string;
+}
 
 export default function Index() {
+
+    const db = getFirestore(app);
+    const [refinanceDocs, setRefinanceDocs] = useState<LoanDoc[]>([]);
+    const [showAll, setShowAll] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+
+            const refinanceCollection = collection(db, "refinance");
+            const refinancneSnapshot = await getDocs(refinanceCollection);
+            const refinanceList: LoanDoc[] = refinancneSnapshot.docs.map((doc) => {
+                const data = doc.data();
+                return {
+                    maxTenure: data.maxTenure,
+                    monthly: data.monthly,
+                    name: data.name,
+                    rate: data.rate,
+                    type: data.type,
+                    url: data.url,
+                };
+            });
+
+            // Update the state
+            setRefinanceDocs(refinanceList);
+        };
+
+        fetchData();
+    }, []);
+
+    const visibleDocs = showAll ? refinanceDocs : refinanceDocs.slice(0, 3);
+
     return (
         <div className="min-h-screen flex flex-col px-6 pb-10 gap-6">
             <div className="text-3xl font-bold">Refinancing Options</div>
@@ -39,48 +84,36 @@ export default function Index() {
                     </Button>
                 </div>
                 <div className="flex flex-col gap-4">
-                    <a
-                        href="https://www.alliancebank.com.my/Personal/Financing/personal-loan-financing"
-                        target="_blank"
-                    >
-                        <Card className="shadow-none">
-                            <CardContent className="px-5 py-4">
-                                <div className="flex flex-col gap-4">
-                                    <div className="flex flex-col gap-1">
-                                        <div className="text-xs text-zinc-500">Housing Loan</div>
-                                        <div>Alliance Bank Personal Loan</div>
+                    {visibleDocs.map((elem) => (
+                        <a
+                            key={elem.url}
+                            href={elem.url}
+                            target="_blank"
+                        >
+                            <Card className="shadow-none">
+                                <CardContent className="px-5 py-4">
+                                    <div className="flex flex-col gap-4">
+                                        <div className="flex flex-col gap-1">
+                                            <div className="text-xs text-zinc-500">{elem.type}</div>
+                                            <div>{elem.name}</div>
+                                        </div>
+                                        <div className="text-3xl font-medium pb-1">{elem.monthly}/mo</div>
+                                        <div className="flex justify-between text-xs text-zinc-500">
+                                            <div>Max {elem.maxTenure}</div>
+                                            <div>{elem.rate}</div>
+                                        </div>
                                     </div>
-                                    <div className="text-3xl font-medium pb-1">RM466.58/mo</div>
-                                    <div className="flex justify-between text-xs text-zinc-500">
-                                        <div>Max 7 years</div>
-                                        <div>from 4.99% p.a.</div>
-                                    </div>
-                                </div>
-                            </CardContent>
-                        </Card>
-                    </a>
-                    <a 
-                        href="https://www.hlb.com.my/en/personal-banking/loans/motor-loan/auto-loans.html/"
-                        target="_blank"
-                    >
-                        <Card className="shadow-none">
-                            <CardContent className="px-5 py-4">
-                                <div className="flex flex-col gap-4">
-                                    <div className="flex flex-col gap-1">
-                                        <div className="text-xs text-zinc-500">Vehicle Loan</div>
-                                        <div>HLB Auto Loan Fixed Rate</div>
-                                    </div>
-                                    <div className="text-3xl font-medium pb-1">RM512.57/mo</div>
-                                    <div className="flex justify-between text-xs text-zinc-500">
-                                        <div>Max 9 years</div>
-                                        <div>2.80% p.a.</div>
-                                    </div>
-                                </div>
-                            </CardContent>
-                        </Card>
-                    </a>
+                                </CardContent>
+                            </Card>
+                        </a>
+                    ))}
                 </div>
-                <div className="text-center font-medium text-zinc-500 text-sm">View more offers</div>
+                {/* <div className="text-center font-medium text-zinc-500 text-sm">View more offers</div> */}
+                {!showAll && (
+                    <div className="text-center font-medium text-zinc-500 text-sm">
+                        <Button onClick={() => setShowAll(true)}>View more offers</Button>
+                    </div>
+                )}
             </div>
         </div>
     );
